434ebb954b6cb5819f979ad9328653175f4cb8bc
HouJie <h.j.13.new@gmail.com>
2025-02-20 20:43:35 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,43 @@
 package main
 
-import (
-	"fmt"
-	"github.com/h-j-13/gopkgs/ut/add"
-)
+import "fmt"
+
+// isBalance 检查 k 是否是p的一个平衡数字
+func isBalance(k int, p []int) bool {
+	// 遍历l~r的区间
+	for l := 0; l < len(p)-k+1; l++ {
+		r := l + k
+		// 检查这个区间是否是1~k的
+		m := make([]int, k)
+		for i := l; i < r; i++ {
+			if p[i] > k {
+				continue // 这个组合不行
+			}
+			m[p[i]-1] = 1 // 通过map记录
+		}
+
+		fmt.Print(p[l:r], ":")
+		fmt.Println(m)
+		// 检查是否是1~k
+		match := true
+		for i := 0; i < k; i++ {
+			if m[i] == 0 {
+				match = false
+				break
+			}
+		}
+
+		if match {
+			return true
+		}
+	}
+	return false
+}
 
 func main() {
-	fmt.Print(add.Add(1, 2))
+	p := []int{4, 1, 3, 2}
+	fmt.Println(isBalance(1, p))
+	fmt.Println(isBalance(2, p))
+	fmt.Println(isBalance(3, p))
+	fmt.Println(isBalance(4, p))
 }
